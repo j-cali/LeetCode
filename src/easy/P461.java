01fcee22e41a41a7d5b7ff7e2065e7b6b09f2662
j-cali <johnkcalilung@gmail.com>
2018-01-29 19:05:19 -0800

    461 - Add comment block info

--- src/easy/P461.java
+++ src/easy/P461.java
@@ -1,3 +1,10 @@
+/**
+ *  Problem: 461. Hamming Distance
+ *  Link: https://leetcode.com/problems/hamming-distance/description/
+ *  Date (MDY): 01/24/2018
+ *  Notes:
+ */
+
 package easy;
 
 public class P461 {
@@ -5,7 +12,7 @@ public class P461 {
 	public static void main(String[] args) {
 		int x = 1;
 		int y = 4;
-		System.out.println(hammingDistance(x, y));
+		System.out.println(hammingDistance(x, y)); // expected answer: 2
 	}
 
     public static int hammingDistance(int x, int y) {
